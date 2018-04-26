5f05e1d4fec01f79f755bb875c0a1c4287b495bb
Administrator <1851656204@qq.com>
2018-04-26 14:36:14 +0800

    我的测试提交

--- hystrix-dashoboard-node1/src/main/java/com/goodonedog/hystrixdashoboardnode1/controller/ConsumerController.java
+++ hystrix-dashoboard-node1/src/main/java/com/goodonedog/hystrixdashoboardnode1/controller/ConsumerController.java
@@ -1,10 +1,21 @@
 package com.goodonedog.hystrixdashoboardnode1.controller;
 
+import com.goodonedog.hystrixdashoboardnode1.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ConsumerController {
 
+    @Autowired
+    private ClientService clientService;
+
+    @RequestMapping(value = "/add",method =  RequestMethod.GET)
+    public Integer sum(@RequestParam("a") Integer a,@RequestParam("b")Integer b) {
+        return clientService.sum(a,b);
+    }
 }
