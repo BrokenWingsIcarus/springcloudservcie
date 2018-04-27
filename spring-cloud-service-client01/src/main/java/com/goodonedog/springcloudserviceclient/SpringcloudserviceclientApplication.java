3ceb707b9bc8705034b4727b35360e94994ed9d3
chinausers <2363001449@qq.com>
2018-04-27 19:50:04 +0800

    成功完成配置中心所有demo 下一个目标 bus 和spring admin ui 目前巨丑无比 期待后续 以后统统改成每天晚上结束后push

--- spring-cloud-service-client01/src/main/java/com/goodonedog/springcloudserviceclient/SpringcloudserviceclientApplication.java
+++ spring-cloud-service-client01/src/main/java/com/goodonedog/springcloudserviceclient/SpringcloudserviceclientApplication.java
@@ -1,11 +1,13 @@
 package com.goodonedog.springcloudserviceclient;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringcloudserviceclientApplication {
 
     public static void main(String[] args) {
---
+++ springcloud-config-server02/src/main/java/com/goodonedog/springcloudconfigserver/SpringcloudConfigserverApplication.java
@@ -0,0 +1,16 @@
+package com.goodonedog.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringcloudConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigserverApplication.class, args);
+	}
+}
